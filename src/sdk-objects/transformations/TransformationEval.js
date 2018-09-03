a811d026884e441c8c16c12bc8b22b8e29a33e96
Aidan Cunniffe <acunniffe@gmail.com>
2018-09-03 13:54:26 -0700

    Local transformation testing

---
+++ src/sdk-objects/transformations/TransformationEval.js
@@ -0,0 +1,105 @@
+import safeEval from 'safe-eval'
+
+export function testTransformation(transformation) {
+	return {
+		'with': (input = {}, answers = {}) => {
+			const withCall = `(${transformation._func.toString()})(input, answers)`
+
+			try {
+				const output = safeEval(withCall, {input, answers, ...helpers()})
+				return {success: true, output}
+			} catch (e) {
+				return {success: false, error: 'Error evaluating code'+ e}
+			}
+		}
+	}
+}
+
+
+const helpers = () => {
+
+	function Generate(schema, value, options) {
+		return {
+			schema: schema,
+			value: value,
+			options: options,
+			_isStagedNode: true
+		}
+	}
+
+	function Mutate(modelId, value, options) {
+		return {
+			modelId: modelId,
+			value: value,
+			options: options,
+			_isStagedMutation: true
+		}
+	}
+
+//Builtins
+
+	function Raw(rawText) {
+		return {
+			schema: 'optic:builtins/raw',
+			value: {
+				rawText: rawText
+			},
+			options: {},
+			_isStagedNode: true
+		}
+	}
+
+
+//Model Setters
+	function Code(value) {
+		return {
+			_valueFormat: 'code',
+			value: value
+		}
+	}
+
+	function Token(value) {
+		return {
+			_valueFormat: 'token',
+			value: value
+		}
+	}
+
+//Container Mutation Enums
+	function Append(items) {
+		return {
+			items: items,
+			type: 'append'
+		}
+	}
+
+	function Prepend(items) {
+		return {
+			items: items,
+			type: 'prepend'
+		}
+	}
+
+	function ReplaceWith(items) {
+		return {
+			items: items,
+			type: 'replace-with'
+		}
+	}
+
+	function InsertAt(index, items) {
+		return {
+			index: index,
+			items: items,
+			type: 'insert-at'
+		}
+	}
+
+	function Empty() {
+		return {
+			type: 'empty'
+		}
+	}
+
+	return {Generate, Mutate, Raw, Code, Token, Append, Prepend, ReplaceWith, InsertAt, Empty}
+}
---
+++ src/sdk-objects/transformations/test/TransformationEval.test.js
@@ -0,0 +1,61 @@
+import assert from 'assert'
+import {testTransformation} from "../TransformationEval";
+import {Transformation} from "../Transformations";
+
+describe('transformation eval', () => {
+
+	it('can run a valid transformation', () => {
+
+		const result = testTransformation(Transformation(
+			'test',
+			'test-id',
+			'input:input',
+			'output:output',
+			(input, answers) => {
+				return {l: true}
+			}
+		)).with({abc: 'def'})
+
+		assert.deepEqual(result, { success: true, output: { l: true } })
+
+	})
+
+	it('can use the helpers', () => {
+
+		const result = testTransformation(Transformation(
+			'test',
+			'test-id',
+			'input:input',
+			'output:output',
+			(input, answers) => {
+				return Generate('abc:def', {l: true})
+			}
+		)).with({abc: 'def'})
+
+		assert.deepEqual(result, { success: true,output:
+				{ schema: 'abc:def',
+					value: { l: true },
+					options: undefined,
+					_isStagedNode: true } })
+
+	})
+
+	it('will return an error if failed', () => {
+
+		const result = testTransformation(Transformation(
+			'test',
+			'test-id',
+			'input:input',
+			'output:output',
+			(input, answers) => {
+				throw new Error('broke it')
+				return {l: true}
+			}
+		)).with({abc: 'def'})
+
+		assert.deepEqual(result, { success: false,
+			error: 'Error evaluating codeError: broke it' })
+
+	})
+
+})
